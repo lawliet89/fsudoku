2e406fb1365c505b82c327d76a11264e505ddd61
Yong Wen Chua <lawliet89@gmail.com>
2016-04-18 23:36:58 +0800

    Add grid file reading code

---
+++ fsudoku.tests/IsValid.fs
@@ -0,0 +1,24 @@
+﻿namespace fsudoku.tests
+
+open NUnit.Framework
+open fsudoku
+
+[<TestFixture>]
+type IsValid() = 
+
+    [<Test>]
+    member this.``returns true on values between 0 and 9 and of length 9``() = 
+        Assert.IsTrue(fsudoku.program.ValidList [for x in 1..9 do yield (Some x)])
+
+    [<Test>]
+    member this.``returns false on values not between 0 and 9``() = 
+        Assert.IsFalse(fsudoku.program.ValidList (Some 999 :: [for x in 1..8 do yield Some(x)]))
+
+    [<Test>]
+    member this.``returns false on a list of length that is not 9``() = 
+        Assert.IsFalse(fsudoku.program.ValidList (Some 9 :: [for x in 1..9 do yield (Some x)]))
+
+    [<Test>]
+    member this.``returns false on a list with duplicates``() =
+        let baseList = [for x in 1..4 do yield (Some x)]
+        Assert.IsFalse(fsudoku.program.ValidList (List.concat [[None; None]; baseList; baseList]))
--- fsudoku/Program.fs
+++ fsudoku/Program.fs
@@ -2,6 +2,7 @@
 
     // Types
     type Value = Option<int>
+    type Grid = Value[,]
 
     let public ValidList (list : Value list) = 
         let rec withinRange = function
@@ -10,9 +11,24 @@
                                                     | None -> true
                                                     | Some x ->  x > 0 && x < 10
                               headWithinRange && withinRange(tail)
-        (list.Length = 9) && (withinRange list)
+        let someList = list |> List.choose id
+        (list.Length = 9) && (withinRange list) && (someList.Length = (List.distinct someList |> List.length))
     
+    let readGrid filename : Grid = 
+       let intToOpt x = match x with
+                            | 0 -> None
+                            | x -> Some x
+       // Is there some better way to do this?
+       let irregular = System.IO.File.ReadLines(filename) 
+                           |> Seq.map (fun line -> 
+                                        line.Split ' '
+                                        |> Seq.map System.Int32.Parse 
+                                        |> Seq.map intToOpt
+                                        |> Seq.toArray)
+                           |> Seq.toArray
+       Array2D.init 9 9 (fun x y -> irregular.[x].[y])
+                                                              
     [<EntryPoint>]
     let main argv = 
-        printfn "%A" argv
+        let grid = readGrid "../../example.txt"
         0 // return an integer exit code

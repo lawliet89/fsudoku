f3ba2fe0751052407ba5d3012fe6ac620bda03a6
Yong Wen Chua <lawliet89@gmail.com>
2016-04-19 00:10:54 +0800

    Add code to print Grid to console

--- fsudoku/Program.fs
+++ fsudoku/Program.fs
@@ -14,7 +14,7 @@
         let someList = list |> List.choose id
         (list.Length = 9) && (withinRange list) && (someList.Length = (List.distinct someList |> List.length))
     
-    let readGrid filename : Grid = 
+    let public ReadGrid filename : Grid = 
        let intToOpt x = match x with
                             | 0 -> None
                             | x -> Some x
@@ -27,8 +27,21 @@
                                         |> Seq.toArray)
                            |> Seq.toArray
        Array2D.init 9 9 (fun x y -> irregular.[x].[y])
+
+    let public GridToString (grid : Grid) = 
+        let intOptionToString = function
+                                | None -> "_"
+                                | Some x -> string x
+        let printRow row = 
+            Array.map intOptionToString row 
+                |> String.concat " " 
+            
+        [for row in 0..8 do yield grid.[row, *]]
+            |> List.map printRow
+            |> String.concat "\n"
                                                               
     [<EntryPoint>]
     let main argv = 
-        let grid = readGrid "../../example.txt"
+        let grid = ReadGrid "../../example.txt"
+        System.Console.Write (GridToString grid)
         0 // return an integer exit code
